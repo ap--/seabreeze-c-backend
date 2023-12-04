07f30d6ca7d1df491d0324d3673e7cc7f4ac5a43
Andreas Poehlmann <ap--@users.noreply.github.com>
2023-12-04 23:18:09 +0100

    seabreeze_c_backend: simplify imports

--- src/seabreeze_c_backend/__init__.py
+++ src/seabreeze_c_backend/__init__.py
@@ -1,47 +1,41 @@
 """This is a wrapper for the c-implementation of the seabreeze-library
 
 """
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeAcquisitionDelayFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeAPI
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeContinuousStrobeFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeDataBufferFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeDevice
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeDHCPServerFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeEEPROMFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeError
 from seabreeze_c_backend._libseabreeze_wrapper import (
+    SeaBreezeAcquisitionDelayFeature,
+    SeaBreezeAPI,
+    SeaBreezeContinuousStrobeFeature,
+    SeaBreezeDataBufferFeature,
+    SeaBreezeDevice,
+    SeaBreezeDHCPServerFeature,
+    SeaBreezeEEPROMFeature,
+    SeaBreezeError,
     SeaBreezeEthernetConfigurationFeature,
-)
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeFastBufferFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeGPIOFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeI2CMasterFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeIntrospectionFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeIPv4Feature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeIrradCalFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeLightSourceFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeMulticastFeature
-from seabreeze_c_backend._libseabreeze_wrapper import (
+    SeaBreezeFastBufferFeature,
+    SeaBreezeFeature,
+    SeaBreezeGPIOFeature,
+    SeaBreezeI2CMasterFeature,
+    SeaBreezeIntrospectionFeature,
+    SeaBreezeIPv4Feature,
+    SeaBreezeIrradCalFeature,
+    SeaBreezeLightSourceFeature,
+    SeaBreezeMulticastFeature,
     SeaBreezeNetworkConfigurationFeature,
-)
-from seabreeze_c_backend._libseabreeze_wrapper import (
     SeaBreezeNonlinearityCoefficientsFeature,
-)
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeNumFeaturesError
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeOpticalBenchFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezePixelBinningFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeRawUSBBusAccessFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeRevisionFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeShutterFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeSpectrometerFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeSpectrumProcessingFeature
-from seabreeze_c_backend._libseabreeze_wrapper import (
+    SeaBreezeNumFeaturesError,
+    SeaBreezeOpticalBenchFeature,
+    SeaBreezePixelBinningFeature,
+    SeaBreezeRawUSBBusAccessFeature,
+    SeaBreezeRevisionFeature,
+    SeaBreezeShutterFeature,
+    SeaBreezeSpectrometerFeature,
+    SeaBreezeSpectrumProcessingFeature,
     SeaBreezeStrayLightCoefficientsFeature,
+    SeaBreezeStrobeLampFeature,
+    SeaBreezeTemperatureFeature,
+    SeaBreezeThermoElectricFeature,
+    SeaBreezeWifiConfigurationFeature,
 )
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeStrobeLampFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeTemperatureFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeThermoElectricFeature
-from seabreeze_c_backend._libseabreeze_wrapper import SeaBreezeWifiConfigurationFeature
 
 try:
     from seabreeze_c_backend._version import __version__ as __version__
@@ -50,18 +44,20 @@ except ImportError:
 
 
 __all__ = [
-    "_backend_",
-    "SeaBreezeAcquisitionDelayFeature",
+    # --- backend ---
+    "__seabreeze_backend__",
     "SeaBreezeAPI",
+    "SeaBreezeDevice",
+    "SeaBreezeError",
+    "SeaBreezeFeature",
+    # --- features ---
+    "SeaBreezeAcquisitionDelayFeature",
     "SeaBreezeContinuousStrobeFeature",
     "SeaBreezeDataBufferFeature",
-    "SeaBreezeDevice",
     "SeaBreezeDHCPServerFeature",
     "SeaBreezeEEPROMFeature",
-    "SeaBreezeError",
     "SeaBreezeEthernetConfigurationFeature",
     "SeaBreezeFastBufferFeature",
-    "SeaBreezeFeature",
     "SeaBreezeGPIOFeature",
     "SeaBreezeI2CMasterFeature",
     "SeaBreezeIntrospectionFeature",
@@ -86,4 +82,4 @@ __all__ = [
     "SeaBreezeWifiConfigurationFeature",
 ]
 
-_backend_ = "cseabreeze"
+__seabreeze_backend__ = _backend_ = "cseabreeze"
